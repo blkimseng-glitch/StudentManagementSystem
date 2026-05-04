1485e23fd49ef034259029c6dcb4df884def7d71
Kimlong Kong <ounlong3699@gmail.com>
2026-05-04 22:08:54 +0700

    update:Kimlong

--- StudentManagementSystem/source/Student.cpp
+++ StudentManagementSystem/source/Student.cpp
@@ -6,6 +6,29 @@
 
 using namespace std;
 
-void addStudent(vector<Student>& students){
+ struct Student
+    {
+        string name,gender;
+        int id;
+        float average;
+    };
     
+
+void addStudent(vector<Student>& students){Student newStudent;
+    cout<< "Enter student Name: ";
+    cin.ignore();
+    getline(cin,newStudent.name);
+
+    cout<< "Enter student Gender: ";
+    cin>>newStudent.gender;
+    
+    cout<< "Enter student ID: ";
+    cin >> newStudent.id;
+
+    cout<< "Enter student Average: ";
+    cin >>newStudent.average;
+
+    students.push_back(newStudent);
+    cout<< "Student added successfully.\n";
+
 }
